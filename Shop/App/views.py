c4dc92011621e8676a2503bbbd21ce2d4b89c0e1
Hobrus <bhobrus@gmail.com>
2024-04-01 20:29:12 +0300

    add token

--- Shop/App/views.py
+++ Shop/App/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 from .serializers import GoodsSerializer
 from .models import Goods
 from abc import ABC
@@ -9,3 +11,4 @@ from abc import ABC
 class GoodsViewSet(viewsets.ModelViewSet):
     serializer_class = GoodsSerializer
     queryset = Goods.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
